72273d8d3b61e3991b9fd274591548abf4dd3f4f
lesenelir <miaomiaobabyzy@gmail.com>
2022-12-01 20:34:24 +0800

    feat: add autiences Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import {Analytics} from "@vercel/analytics/dist/react"
+
 import '../styles/globals.css'
 import '../styles/md.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Component {...pageProps}/>
+      <Analytics/>
+    </>
+  )
 }
 
 export default MyApp
