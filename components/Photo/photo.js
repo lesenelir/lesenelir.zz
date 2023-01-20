ac9223fb043da82fe09b93f9f76fff265e025e90
lesenelir <miaomiaobabyzy@gmail.com>
2023-01-20 15:24:21 +0800

    fix: photo component in dark theme

--- components/Photo/photo.js
+++ components/Photo/photo.js
@@ -8,28 +8,30 @@ import styles from '../../styles/photo.module.css'
 function Photo() {
 
   return (
-    <>
-      <p className={styles.date}>2022-11-30</p>
-      <Item currentDate='2022-11-30'/>
+    <div className={styles.box}>
+      <div>
+        <p className={styles.date}>2022-11-30</p>
+        <Item currentDate='2022-11-30'/>
 
-      <p className={styles.date}>2022-11-28</p>
-      <Item currentDate='2022-11-28'/>
+        <p className={styles.date}>2022-11-28</p>
+        <Item currentDate='2022-11-28'/>
 
-      <p className={styles.date}>2022-03-04</p>
-      <Item currentDate='2022-03-04'/>
+        <p className={styles.date}>2022-03-04</p>
+        <Item currentDate='2022-03-04'/>
 
-      <p className={styles.date}>2021-07-11</p>
-      <Item currentDate='2021-07-11'/>
+        <p className={styles.date}>2021-07-11</p>
+        <Item currentDate='2021-07-11'/>
 
-      <p className={styles.date}>2021-04-24</p>
-      <Item currentDate='2021-04-24'/>
+        <p className={styles.date}>2021-04-24</p>
+        <Item currentDate='2021-04-24'/>
 
-      <p className={styles.date}>2021-01-28</p>
-      <Item currentDate='2021-01-28'/>
+        <p className={styles.date}>2021-01-28</p>
+        <Item currentDate='2021-01-28'/>
 
-      <Cd/>
-      <Footer/>
-    </>
+        <Cd/>
+        <Footer/>
+      </div>
+    </div>
   )
 }
 
