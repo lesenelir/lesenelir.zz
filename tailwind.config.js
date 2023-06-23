8d9e4c50368953fa4e55ae186063a99f2ef8a2fb
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-23 23:42:03 +0800

    feat: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const colors = require("tailwindcss/colors")
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
