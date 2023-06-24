01f45c5dcd4615c66f39f865bf363b537d9b0c57
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-24 21:59:10 +0800

    feat: footer component

---
+++ src/components/utils/Footer.tsx
@@ -0,0 +1,19 @@
+function Footer() {
+  return (
+    <>
+      <p className={'flex flex-row gap-2 mt-8 text-sm'}>
+        <a
+          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+          rel='noreferrer'
+          target='_blank'
+          className={'border-solid border-b-[1px] border-b-borderUnderline border-opacity-30 hover:border-opacity-60'}
+        >
+          CC BY-NC-SA 4.0
+        </a>
+        {' '} 2022-PRESENT © Lesenelir Zhou
+      </p>
+    </>
+  )
+}
+
+export default Footer
