c99a57d79574d2616b5875bcec6244952eed989d
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-24 22:04:38 +0800

    feat: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,16 @@
+import Cd from "../components/utils/Cd"
+import Footer from "../components/utils/Footer"
+
 function NotFoundPage() {
   return (
-    <>
-      404
-    </>
+    <div className={'flex flex-row justify-center'}>
+      <div className={'p-8'}>
+        <h1 className={'text-3xl font-bold mb-10 text-black dark:text-white'}>404</h1>
+        <p className={'text-base mb-10'}>Nice to meet you!</p>
+        <Cd/>
+        <Footer/>
+      </div>
+    </div>
   )
 }
 
