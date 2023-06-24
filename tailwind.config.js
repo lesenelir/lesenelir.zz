fc7ea2573a1a3ad3bab59ad40619e0da9b5e3ba7
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-24 21:25:46 +0800

    feat: add dark mode in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,22 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'comic': ['Comic Sans MS', 'sans-serif'],
+        'icon': ['iconfont', 'sans-serif'],
+      },
+      colors: {
+        // navbar right side
+        navbarLight: '#374151',
+        navbarDark: '#E5E7EB',
+        // main text color
+        textLight: '#555555',
+        textDark: '#BBBBBB',
+      }
+    },
   },
   variants: {
     extend: {},
