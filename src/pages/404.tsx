6fb2cff0385706607b37cfafc6cbe357f7a0564c
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-25 17:31:51 +0800

    feat: change padding p-6

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Footer from "../components/utils/Footer"
 function NotFoundPage() {
   return (
     <div className={'flex flex-row justify-center'}>
-      <div className={'p-4'}>
+      <div className={'p-6'}>
         <h1 className={'text-3xl font-bold mb-10 text-black dark:text-white'}>404</h1>
         <p className={'text-base mb-10'}>Nice to meet you!</p>
         <Cd/>
