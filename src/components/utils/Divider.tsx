775d18b590c65dc6477e4ec3ad4109351ef9e362
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-25 17:31:25 +0800

    chore: margin my-6

--- src/components/utils/Divider.tsx
+++ src/components/utils/Divider.tsx
@@ -1,6 +1,6 @@
 function Divider() {
   return (
-    <div className={'w-1/2 h-px bg-borderUnderline opacity-30 my-8 mx-auto'}/>
+    <div className={'w-1/2 h-px bg-borderUnderline opacity-30 my-6 mx-auto'}/>
   )
 }
 
