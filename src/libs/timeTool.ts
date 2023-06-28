0b11ec36af56a3648d7be0d878acead6528de230
lesenelir <miaomiaobabyzy@gmail.com>
2023-06-28 18:11:07 +0800

    feat: timeline page

---
+++ src/libs/timeTool.ts
@@ -0,0 +1,36 @@
+import path from 'node:path'
+import fsPromises from 'node:fs/promises'
+import matter from "gray-matter"
+import {unified} from 'unified'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+
+async function getTimePost() {
+  const filePath = path.join(process.cwd(), 'public', 'timeline', 'timeline.md')
+  const file = await fsPromises.readFile(filePath, {
+    encoding: 'utf-8'
+  })
+
+  const matterResult: matter.GrayMatterFile<string> = matter(file)
+
+  const fileContent = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process(matterResult.content)
+
+  const contentHtml = fileContent.value
+
+  return {
+    contentHtml,
+    ...matterResult.data
+  }
+}
+
+export {
+  getTimePost
+}
+
--- src/pages/timeline/index.tsx
+++ src/pages/timeline/index.tsx
@@ -1,7 +1,51 @@
-function TimelinePage() {
+import Head from "next/head"
+import {GetStaticProps} from "next"
+
+import {ITimelineData} from "../../libs/types"
+import {getTimePost} from "../../libs/timeTool"
+import Cd from "../../components/utils/Cd";
+import Footer from "../../components/utils/Footer";
+
+interface IProps {
+  timelineData: ITimelineData
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const timelineData = await getTimePost()
+
+  return {
+    props: {
+      timelineData
+    }
+  }
+}
+
+function TimelinePage(props: IProps) {
+  const {timelineData} = props
+
   return (
     <>
-      timeline page
+      <Head>
+        <title>Timeline - Lesenelir Zhou</title>
+        <meta name="description" content="Lesenelir's Blog built by Next.js"/>
+        <link rel="icon" href={`/favicon.ico`}/>
+      </Head>
+
+      <div className={'flex flex-row justify-center'}>
+        <div className={'p-6 max-w-3xl'}>
+          {/* Content */}
+          <h1 className={'text-3xl text-textLightStrong font-bold dark:text-textDarkStrong'}>Timeline</h1>
+          <article className={'prose dark:prose-d ark'}>
+            <div dangerouslySetInnerHTML={{__html: timelineData.contentHtml}}/>
+          </article>
+
+          {/* Page Footer */}
+          <div className={'mt-8'}>
+            <Cd/>
+            <Footer/>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
